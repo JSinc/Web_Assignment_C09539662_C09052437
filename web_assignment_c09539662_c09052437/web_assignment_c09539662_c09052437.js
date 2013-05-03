acded671ae1f2f5b72a1486760d3150e903a8813
Jody Sinclair <jodyshouse@hotmail.com>
2013-05-03 01:19:23 +0100

    fix

--- web_assignment_c09539662_c09052437/web_assignment_c09539662_c09052437.js
+++ web_assignment_c09539662_c09052437/web_assignment_c09539662_c09052437.js
@@ -1,24 +1,3 @@
-/*if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to web_assignment_c09539662_c09052437.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
-*/
-
 // Set up a collection to contain player information. On the server,
 // it is backed by a MongoDB collection named "players".
 
@@ -26,89 +5,77 @@ Questions = new Meteor.Collection("questions");
 
 if (Meteor.isClient) {
 	
-  Template.web_assignment_c09539662_c09052437.questions = function () {
-    return Questions.find({}, {sort: {score: -1, q: 1, desc: 1}});
-  };
-
-  Template.web_assignment_c09539662_c09052437.selected_q = function () {
-    var question = Questions.findOne(Session.get("selected_q"));
-    return question && question.q ;
-  };
-    Template.web_assignment_c09539662_c09052437.selected_desc = function () {
-    var question = Questions.findOne(Session.get("selected_q"));
-    return question && question.desc ;
-  };
-
-  Template.question.selected = function () {
-    return Session.equals("selected_q", this._id) ? "selected" : '';
-  };
-
-  Template.web_assignment_c09539662_c09052437.events({
-    'click Button.inc': function () {
-      Questions.update(Session.get("selected_q"), {$inc: {score: 1}});
-    }
-	});
-	Template.web_assignment_c09539662_c09052437.events({
-	'click Button.dec': function () {
-      Questions.update(Session.get("selected_q"), {$inc: {score: -1}});
-    }
-  });
-    Template.web_assignment_c09539662_c09052437.events({
-	'click Button.add': function () {
-	var newQ = document.getElementById("newQ").value; 
-	var newD = document.getElementById("newD").value;
-	if (newQ.length < 80)
-	{
-	Questions.insert({q: newQ, desc: newD, score: 0});
-	}
-	else
-	{
-		window.alert("Question title is too long");
-	}
-	}
-	});
-	Template.web_assignment_c09539662_c09052437.events({
-	'click Button.del': function ()
-	{ 
-	Questions.remove(Session.get("selected_q"));
-	}
-	});
-
-  Template.question.events({
-    'click': function () {
-      Session.set("selected_q", this._id);
-    }
-  });
-  /*
-  Template.login({
-  function() {
-       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-       po.src = 'https://apis.google.com/js/client:plusone.js';
-       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-     })();*/
-}
-
+	Template.web_assignment_c09539662_c09052437.questions = function () {
+		return Questions.find({}, {sort: {score: -1, q: 1, desc: 1}});
+	};
 	
+	Template.web_assignment_c09539662_c09052437.selected_q = function () {
+		var question = Questions.findOne(Session.get("selected_q"));
+		return question && question.q ;
+	};
+	
+	Template.web_assignment_c09539662_c09052437.selected_desc = function () {
+		var question = Questions.findOne(Session.get("selected_q"));
+		return question && question.desc ;
+	};
+	
+	Template.question.selected = function () {
+		return Session.equals("selected_q", this._id) ? "selected" : '';
+	};
+	
+	Template.web_assignment_c09539662_c09052437.events({
+		'click Button.inc': function () {
+	  		Questions.update(Session.get("selected_q"), {$inc: {score: 1}});
+		}
+	});
+	Template.web_assignment_c09539662_c09052437.events({
+		'click Button.dec': function () {
+	  		Questions.update(Session.get("selected_q"), {$inc: {score: -1}});
+		}
+	});
+	
+	Template.web_assignment_c09539662_c09052437.events({
+		'click Button.add': function () {
+			var newQ = document.getElementById("newQ").value; 
+			var newD = document.getElementById("newD").value;
+			if (newQ.length < 80){
+				Questions.insert({q: newQ, desc: newD, score: 0});
+			}
+			else
+			{
+				window.alert("Question title is too long");
+			}
+		}
+	});
+	
+	Template.web_assignment_c09539662_c09052437.events({
+		'click Button.del': function () { 
+			Questions.remove(Session.get("selected_q"));
+		}
+	});
+	
+	Template.question.events({
+		'click': function () {
+	 		 Session.set("selected_q", this._id);
+		}
+	});
 
-// On server startup, create some players if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-						   // first, remove configuration entry in case service is already configured
-Accounts.loginServiceConfiguration.remove({
-  service: "google"
-});
-
-    if (Questions.find().count() === 0) {
-      var qs = ["Alri?",
-                   "Story?",
-                   "spa",
-                   "yeah"];
-      for (var i = 0; i < qs.length; i++)
-        Questions.insert({q: qs[i], desc: "", score: Math.floor(Random.fraction()*10)*5});
-    }
-  });
 }
 
+if (Meteor.isServer) {
+	Meteor.startup(function () {
 
+		//Remove configuration entry
+		Accounts.loginServiceConfiguration.remove({
+			service: "google"
+		});
 
-
+		if (Questions.find().count() === 0) {
+			var qs = ["Alri?","Story?","spa","yeah"];
+		
+			for (var i = 0; i < qs.length; i++) {
+				Questions.insert({q: qs[i], desc: "", score: Math.floor(Random.fraction()*10)*5});
+			}
+		}
+	});
+}
