bb436081bef084bc9b6b7fb2e11a5d02fb878ee6
Jody Sinclair <jodyshouse@hotmail.com>
2013-05-06 23:10:25 +0100

    this

--- web_assignment_c09539662_c09052437/web_assignment_c09539662_c09052437.js
+++ web_assignment_c09539662_c09052437/web_assignment_c09539662_c09052437.js
@@ -1,9 +1,16 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
+// Rich Web Assignment - DT 228/4
+// Jody Sinclair - C09539662 & Keith Hagan - C09052437
+// Application for students to ask questions in class and vote them up
+// or down allowing the lecturer to view the most requested question.
+// Users can only vote up and down 5 times each and must sign in before
+// viewing or accessing content.
 
 Questions = new Meteor.Collection("questions");
 
 if (Meteor.isClient) {
+	
+	var votedUp = 0;
+	var votedDown = 0;
 	
 	Template.web_assignment_c09539662_c09052437.questions = function () {
 		return Questions.find({}, {sort: {score: -1, q: 1, desc: 1}});
@@ -25,21 +32,39 @@ if (Meteor.isClient) {
 	
 	Template.web_assignment_c09539662_c09052437.events({
 		'click Button.inc': function () {
-	  		Questions.update(Session.get("selected_q"), {$inc: {score: 1}});
+			if (votedUp < 5)
+			{
+		  		Questions.update(Session.get("selected_q"), {$inc: {score: 1}});
+				votedUp++;
+			}
+			else
+			{
+				window.alert("You may only vote up 5 times per session");
+			}
 		}
 	});
 	Template.web_assignment_c09539662_c09052437.events({
 		'click Button.dec': function () {
-	  		Questions.update(Session.get("selected_q"), {$inc: {score: -1}});
+			if (votedDown < 5)
+			{
+		  		Questions.update(Session.get("selected_q"), {$inc: {score: -1}});
+				votedDown++;
+			}
+			else
+			{
+				window.alert("You may only vote down 5 times per session");
+			}
 		}
 	});
 	
 	Template.web_assignment_c09539662_c09052437.events({
-		'click Button.add': function () {
+		'click span.add': function () {
 			var newQ = document.getElementById("newQ").value; 
 			var newD = document.getElementById("newD").value;
 			if (newQ.length < 80){
 				Questions.insert({q: newQ, desc: newD, score: 0});
+				document.getElementById("newQ").value = '';
+				document.getElementById("newD").value = '';
 			}
 			else
 			{
@@ -49,7 +74,7 @@ if (Meteor.isClient) {
 	});
 	
 	Template.web_assignment_c09539662_c09052437.events({
-		'click Button.del': function () { 
+		'click span.del': function () { 
 			Questions.remove(Session.get("selected_q"));
 		}
 	});
@@ -68,6 +93,11 @@ if (Meteor.isServer) {
 		//Remove configuration entry
 		Accounts.loginServiceConfiguration.remove({
 			service: "google"
+		});
+		Accounts.loginServiceConfiguration.insert({
+			service: "google",
+			clientId: "783577748341-0ral1ci40fmrdn87u23lubvcbok0j0de.apps.googleusercontent.com",
+			secret: "IffJHL8WyoITbO_5lz9yT2Kr"
 		});
 
 		if (Questions.find().count() === 0) {
